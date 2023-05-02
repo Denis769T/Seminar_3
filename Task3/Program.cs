3da5aa7b9719ace1d62d0d376cd667d780c83e6b
Denis <denis-topalov@yandex.ru>
2023-05-03 06:46:56 +1100

    Seminar_3. Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Write("Введите значение N=");
+int N = Convert.ToInt32(Console.ReadLine());
+for (int A = 1; A <= N; A++)
+{
+    if (A == N)
+    {
+        double C = Math.Pow(A, 3); //Возведение в третью степень.
+        Console.WriteLine($"{C} ");
+        return;
+    }
+
+    double B = Math.Pow(A, 3); //Возведение в третью степень.
+    Console.Write($"{B}, ");
+
+}
